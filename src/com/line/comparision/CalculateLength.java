1398ccc489bdd9da2f124f04dcc86fb18520b237
basha <bashamb787@gmail.com>
2022-02-05 14:44:29 +0530

    Line Comparision using object conscepts

--- src/com/line/comparision/CalculateLength.java
+++ src/com/line/comparision/CalculateLength.java
@@ -10,5 +10,58 @@ public class CalculateLength {
          int Line = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
          System.out.println( "Length of line is: "+Line);
 	}
+	
+	public void findEquality() {
+		int x1 = 5;
+		int x2 = 10;
+		int x3 = 5;
+		int x4 = 2;
+		int y1 = 5;
+		int y2 = 10;
+		int y3 = 5;
+		int y4 = 2;
+		
+		int lineOne = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
+		int lineTwo = ((((x4 -x3)*(x4-x3))+((y4-y3)+(y4-y3))));
+		
+		String lengthOne = String.valueOf(lineOne);
+		String lengthTwo = String.valueOf(lineTwo);
+		
+		System.out.println("Length of first Line :"+lineOne);
+		System.out.println("Length of Second Line :" +lineTwo);
+		System.out.println(lengthOne.equals(lengthTwo));
+	}
+	
+	public void checkCompareLine() {
+		
+		int x1 = 5;
+		int x2 = 10;
+		int x3 = 15;
+		int x4 = 20;
+		int y1 = 55;
+		int y2 = 100;
+		int y3 = 25;
+		int y4 = 28;
+		int lineOne = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
+		int lineTwo = ((((x4 -x3)*(x4-x3))+((y4-y3)+(y4-y3))));
+	
+		System.out.println(lineOne); 
+		System.out.println(lineTwo); 
+			if(lineOne > lineTwo){
+		System.out.println("Line one is greater:" +lineOne);
+
+		}
+			else if(lineTwo > lineOne){
+
+             System.out.println("Line Two is greater:" +lineTwo);
+
+            }
+             else {
+             System.out.println("Both Lines are equal:");
+
+			}
+		
+	}
+
 
 }
--- src/com/line/comparision/GeometryMain.java
+++ src/com/line/comparision/GeometryMain.java
@@ -3,15 +3,10 @@ package com.line.comparision;
 public class GeometryMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		CalculateLength cl = new CalculateLength();
 		cl.getLength();
-		
-		CheckEquality ce = new CheckEquality();
-		ce.findEquality();
-//		
-		CompareLine campare = new CompareLine();
-		campare.checkCompareLine();
+		cl.findEquality();
+		cl.checkCompareLine();
 	}
 
 }
