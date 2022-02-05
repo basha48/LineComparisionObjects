710950744d6c0285bf1570732f3646422d5f7a70
basha <69071214+basha48@users.noreply.github.com>
2022-02-05 14:46:26 +0530

    Calcualte Length

--- src/com/line/comparision/CalculateLength.java
+++ src/com/line/comparision/CalculateLength.java
@@ -4,11 +4,11 @@ public class CalculateLength {
 	
 	public void getLength() {
 		 int x1 = 5;
-         int x2 = 10;
-         int y1 = 5;
-         int y2 = 2;
-         int Line = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
-         System.out.println( "Length of line is: "+Line);
+         	 int x2 = 10;
+        	 int y1 = 5;
+         	 int y2 = 2;
+         	 int Line = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
+        	 System.out.println( "Length of line is: "+Line);
 	}
 	
 	public void findEquality() {
@@ -49,17 +49,13 @@ public class CalculateLength {
 		System.out.println(lineTwo); 
 			if(lineOne > lineTwo){
 		System.out.println("Line one is greater:" +lineOne);
-
 		}
-			else if(lineTwo > lineOne){
-
+			else if(lineTwo > lineOne) {
              System.out.println("Line Two is greater:" +lineTwo);
-
-            }
+               }
              else {
              System.out.println("Both Lines are equal:");
-
-			}
+	     }
 		
 	}
 
